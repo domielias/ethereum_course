9ca052d66eeaf41c853cf51bbcb868960d6492a3
Elias Rodriguez <elflautista321@gmail.com>
2022-06-08 00:04:41 -0400

    Uploaded first contract

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,31 @@
 // deploy code will go here
+const HdWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+require('dotenv').config()
+
+const provider = new HdWalletProvider(
+  process.env.MNEMONIC,
+  process.env.POCKET_ENDPOINT
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  // Get accouns from Mnemonic
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts[0]);
+  // Keep the response from the contract creation
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+                    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+                    .send(({ from: accounts[0], gas: '1000000' }));
+  // Return the address of the contract
+  console.log('Contract deployed to: ', result.options.address);
+  // Test of the getting the message from contract
+  const message = await result.methods.message().call();
+  console.log('Value of the message method',message );
+  const setMessage = await result.methods.setMessage('Bye there!').send({ from: accounts[0] });
+  const messageFromSetMessage = await result.methods.message().call();
+  console.log('Value of the setMessage method',messageFromSetMessage );
+  provider.engine.stop();
+}
+deploy();
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,1 +1,33 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider()); //Create instance from web3 and connect with the ganache provider network.
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+  //get a list of all accounts  
+  accounts = await web3.eth.getAccounts();
+  //use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+                        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+                        .send(({ from: accounts[0], gas: '1000000' }));
+});
+
+describe('Inbox', () => {
+  it('should deploy a contract', () => {
+    //Check if the value of the contract is defined
+    assert.ok(inbox.options.address); 
+  });
+  it('should have a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
+  it('should set the message', async () => {
+    await inbox.methods.setMessage('Bye there!').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Bye there!');
+  });
+});
